57340936be92d65502310c8a4f78f127e86b22e8
Destranix <meinphp@gmx.de>
2023-01-30 14:47:33 +0100

    Reformated

--- src/car_simulator/src/transformations/tf_remap.cpp
+++ src/car_simulator/src/transformations/tf_remap.cpp
@@ -5,50 +5,50 @@
 
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "tf2_ros/transform_broadcaster.h"
-#include "tf2_ros/static_transform_broadcaster.h"
 #include "tf2_msgs/msg/tf_message.hpp"
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "tf2_ros/transform_broadcaster.h"
 
-class TFRemapper : public rclcpp::Node
-{
-private:
+class TFRemapper : public rclcpp::Node {
+   private:
 	rclcpp::Subscription<tf2_msgs::msg::TFMessage>::SharedPtr subscription_;
-	rclcpp::Subscription<tf2_msgs::msg::TFMessage>::SharedPtr static_subscription_;
-	
+	rclcpp::Subscription<tf2_msgs::msg::TFMessage>::SharedPtr
+		static_subscription_;
+
 	rclcpp::Publisher<tf2_msgs::msg::TFMessage>::SharedPtr tf_publisher_;
 	rclcpp::Publisher<tf2_msgs::msg::TFMessage>::SharedPtr static_tf_publisher_;
-	
+
 	tf2_msgs::msg::TFMessage net_message_;
-	
+
 	std::string src_;
 	std::string dst_;
-	
+
 	std::string src_tf_;
 	std::string dst_tf_;
-	
+
 	std::string src_tf_static_;
 	std::string dst_tf_static_;
-public:
+
+   public:
 	TFRemapper()
-	: Node("frame_remap")
-	{
+		: Node("frame_remap") {
 		src_ = this->declare_parameter("src", "");
 		dst_ = this->declare_parameter("dst", "");
-		
+
 		src_tf_ = this->declare_parameter("src_tf", "tf");
 		dst_tf_ = this->declare_parameter("dst_tf", "tf");
-		
+
 		src_tf_static_ = this->declare_parameter("src_tf_static", "tf_static");
 		dst_tf_static_ = this->declare_parameter("dst_tf_static", "tf_static");
 
 		// Initialize the transform broadcaster
-		tf_publisher_ = create_publisher<tf2_msgs::msg::TFMessage>(
-			dst_tf_.c_str(),
-			100
-		);
+		tf_publisher_ = create_publisher<tf2_msgs::msg::TFMessage>(dst_tf_.c_str(), 100);
 		static_tf_publisher_ = create_publisher<tf2_msgs::msg::TFMessage>(
 			dst_tf_static_.c_str(),
-			rclcpp::QoS(1).keep_all().transient_local()//Ensure nodes that subscribe late still receive static transformation
+			rclcpp::QoS(1)
+				.keep_all()
+				.transient_local()// Ensure nodes that subscribe late still receive
+								  // static transformation
 		);
 
 		RCLCPP_INFO_THROTTLE(
@@ -69,30 +69,29 @@ public:
 			10,
 			std::bind(&TFRemapper::handle_pose<false>, this, std::placeholders::_1)
 		);
-		
+
 		static_subscription_ = this->create_subscription<tf2_msgs::msg::TFMessage>(
 			src_tf_static_.c_str(),
 			10,
 			std::bind(&TFRemapper::handle_pose<true>, this, std::placeholders::_1)
 		);
-		//TODO:Maybe use filter
+		// TODO:Maybe use filter
 	}
 
-private:
-	void sendStaticTransform(const tf2_msgs::msg::TFMessage& msg){
-		for(const geometry_msgs::msg::TransformStamped& current_transform : msg.transforms){
+   private:
+	void sendStaticTransform(const tf2_msgs::msg::TFMessage& msg) {
+		for(const geometry_msgs::msg::TransformStamped& current_transform : msg.transforms) {
 			auto predicate = [&current_transform](const geometry_msgs::msg::TransformStamped existing) {
 				return current_transform.child_frame_id == existing.child_frame_id;
 			};
 			auto existing = std::find_if(net_message_.transforms.begin(), net_message_.transforms.end(), predicate);
 
-			if (existing != net_message_.transforms.end()){
+			if(existing != net_message_.transforms.end()) {
 				*existing = current_transform;
-			}else{
+			} else {
 				net_message_.transforms.push_back(current_transform);
-				
-				RCLCPP_INFO_THROTTLE(
-					get_logger(),
+
+				RCLCPP_INFO_THROTTLE(get_logger(),
 					*get_clock(),
 					std::chrono::milliseconds(1000).count(),
 					"Statically mapping from %s to %s",
@@ -101,58 +100,59 @@ private:
 				);
 			}
 		}
-		
+
 		this->static_tf_publisher_->publish(net_message_);
 	}
 
 	/*
-	 * TODO: If required maybe map src/a->src/b to src/a->dst/a->src/b->dst/b
-	 */
+   * TODO: If required maybe map src/a->src/b to src/a->dst/a->src/b->dst/b
+   */
 	template<bool is_static>
-	void handle_pose(const std::shared_ptr<tf2_msgs::msg::TFMessage> msg)
-	{
-		for(const geometry_msgs::msg::TransformStamped& current_transform : msg->transforms){
-			//Only map messages with frames from our namespace
-			//We resend the original message together with a mapping to our new namespace
-			if(current_transform.header.frame_id.find(src_) != std::string::npos || current_transform.child_frame_id.find(src_) != std::string::npos){
-
+	void handle_pose(const std::shared_ptr<tf2_msgs::msg::TFMessage> msg) {
+		for(const geometry_msgs::msg::TransformStamped& current_transform:
+			msg->transforms) {
+			// Only map messages with frames from our namespace
+			// We resend the original message together with a mapping to our new
+			// namespace
+			if(current_transform.header.frame_id.find(src_) != std::string::npos || current_transform.child_frame_id.find(src_) != std::string::npos) {
 				tf2_msgs::msg::TFMessage send_message_static;
-				
-				if(current_transform.header.frame_id.find(src_) != std::string::npos){
+
+				if(current_transform.header.frame_id.find(src_) != std::string::npos) {
 					geometry_msgs::msg::TransformStamped t_link;
-					t_link.header.stamp = current_transform.header.stamp;
+					t_link.header.stamp	   = current_transform.header.stamp;
 					t_link.header.frame_id = current_transform.header.frame_id;
 					t_link.child_frame_id = (dst_ + current_transform.header.frame_id.substr(src_.length()));
-					
+
 					send_message_static.transforms.push_back(t_link);
 				}
-				
-				if(current_transform.child_frame_id.find(src_) != std::string::npos){
+
+				if(current_transform.child_frame_id.find(src_) != std::string::npos) {
 					geometry_msgs::msg::TransformStamped t_link;
-					t_link.header.stamp = current_transform.header.stamp;
+					t_link.header.stamp	   = current_transform.header.stamp;
 					t_link.header.frame_id = current_transform.child_frame_id;
 					t_link.child_frame_id = (dst_ + current_transform.child_frame_id.substr(src_.length()));
-					
+
 					send_message_static.transforms.push_back(t_link);
 				}
-				
 
 				// Send the transformation
-				if(is_static){
+				if(is_static) {
 					send_message_static.transforms.push_back(current_transform);
-				}else{
+				} else {
 					tf2_msgs::msg::TFMessage send_message;
 					send_message.transforms.push_back(current_transform);
 					this->tf_publisher_->publish(send_message);
 				}
-				this->sendStaticTransform(send_message_static);//TODO:Maybe first sort out already seen frames
+				this->sendStaticTransform(
+					send_message_static
+				);	// TODO:Maybe first sort out already seen
+					// frames
 			}
 		}
 	}
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char* argv[]) {
 	rclcpp::init(argc, argv);
 	rclcpp::spin(std::make_shared<TFRemapper>());
 	rclcpp::shutdown();
