3b35da051e37ff395b48309b1d1ff998f2660f43
Sajad Schaheer <sajad.schaheer@tu-dortmund.de>
2023-08-25 12:14:49 +0200

    Communication: ignoring selfmessages

--- src/communication/src/publisher_node.cpp
+++ src/communication/src/publisher_node.cpp
@@ -3,25 +3,21 @@
 #include "std_msgs/msg/string.hpp"
 #include "autoware_auto_vehicle_msgs/msg/velocity_report.hpp"
 
-rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher;
 std::vector<std_msgs::msg::String> messages;
 
 
+
+// Storing Velocity Report in a Queue (messages). We do not want to pushlish every message directly, but have use scheduled publishing 
 void velocityReportCallback(const autoware_auto_vehicle_msgs::msg::VelocityReport::SharedPtr msg, const std::string& robotName)
 {
         double longitudinalVelocity = msg->longitudinal_velocity;
         double lateralVelocity = msg->lateral_velocity;
-        //RCLCPP_INFO(rclcpp::get_logger("subscriber_node"), "Velocity Report: Longitudinal=%.2f, Lateral=%.2f",
-        //                                                              longitudinalVelocity, lateralVelocity);
-        //auto message = std_msgs::msg::String();
         std_msgs::msg::String message;
         message.data = "Robot "+ robotName +"'s velocity report: , Long = " + std::to_string(longitudinalVelocity)+
                 ", Lateral = " + std::to_string(lateralVelocity);
         messages.push_back(message);
-        //RCLCPP_INFO(rclcpp::get_logger("subscription_node"), "published '%s'", message.data.c_str());
-        //publisher -> publish(message);
-
 }
+
 
 
 int main(int argc, char** argv)
@@ -38,40 +34,28 @@ int main(int argc, char** argv)
                     return 1;
             }
 
-
+	//creating Publisher Node 
         auto node = rclcpp::Node::make_shared("publisher_node");
-        publisher = node->create_publisher<std_msgs::msg::String>("/topic", 10);
+	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher;
+        publisher = node->create_publisher<std_msgs::msg::String>("/comm_topic", 10);
 
-        std::vector<rclcpp::Subscription<autoware_auto_vehicle_msgs::msg::VelocityReport>::SharedPtr> subscriptions;
+        //int numRobots = std::stoi(robotNumber);
 
-        int numRobots = std::stoi(robotNumber);
-        messages.resize(numRobots);
-
-        //for(int i = 0; i < numRobots; ++i) {
-        //      std::string robotName = "robot" + std::to_string(i);
-        //      std::string robotTopic = "/"+robotName+ "/vehicle/status/velocity_status";
-        //      auto subscription = node -> create_subscription<autoware_auto_vehicle_msgs::msg::VelocityReport>(
-        //                      robotTopic, 10,
-        //                      [robotName] (const autoware_auto_vehicle_msgs::msg::VelocityReport::SharedPtr msg) {
-        //                      velocityReportCallback(msg,robotName);
-        //                      });
-        //      subscriptions.push_back(subscription);
-        //      RCLCPP_INFO(node -> get_logger(), "Subscription created on %s", robotTopic.c_str());
-        //}
-        //
-        //
-        std::string robotTopic = "/"+robotName+ "/vehicle/status/velocity_status";
-        //auto subscription = node->create_subscription<autoware_auto_vehicle_msgs::msg::VelocityReport>(
-        //                                      "/robot0/vehicle/status/velocity_status",10,velocityReportCallback);
-
-        auto subscription = node -> create_subscription<autoware_auto_vehicle_msgs::msg::VelocityReport>(
+        
+	//creating Subscription for Velocity Report
+	std::string robotTopic = "/"+robotName+ "/vehicle/status/velocity_status";
+	auto subscription = node -> create_subscription<autoware_auto_vehicle_msgs::msg::VelocityReport>(
                               robotTopic, 10,
                               [robotName] (const autoware_auto_vehicle_msgs::msg::VelocityReport::SharedPtr msg) {
                               velocityReportCallback(msg,robotName);
                               });
         //RCLCPP_INFO(node->get_logger(), "Subscription created on vehicle/status/velocity_status");
 
-        rclcpp::WallRate loop_rate(0.2); // 0.2 Hz (every 5 seconds)
+        
+	
+
+	//Publishing Messages from the Queue 
+	rclcpp::WallRate loop_rate(0.2); // 0.2 Hz (every 5 seconds)
         //int count = 0;
         while (rclcpp::ok()) {
                 for (const auto& message : messages) {
@@ -82,8 +66,6 @@ int main(int argc, char** argv)
                 }
 
                 messages.clear();
-                messages.resize(numRobots);
-
                 rclcpp::spin_some(node);
                 loop_rate.sleep();
         }
--- src/communication/src/subscriber_node.cpp
+++ src/communication/src/subscriber_node.cpp
@@ -3,8 +3,20 @@
 
 void callback(const std_msgs::msg::String::SharedPtr message, const std::string& robotName)
 {
-	  RCLCPP_INFO(rclcpp::get_logger("subscriber_node"), "'%s' received: '%s'",robotName.c_str(), message->data.c_str());
+	// Check if the message contains the robot's name (ignore self messages) 
+    	if (message->data.find(robotName) != std::string::npos) {
+        	//RCLCPP_INFO(rclcpp::get_logger("subscriber_node"), "Ignoring message with robot name: '%s'", robotName.c_str());
+        	return;
+    	}  
+	RCLCPP_INFO(rclcpp::get_logger("subscriber_node"), "'%s' received: '%s'",robotName.c_str(), message->data.c_str());
+
+
+	//future works:
+	//store the messages in a queue and process them to improve the performance 
+	//Having velocity, direction and postion of other cars can contribute to a better estimation of the robot's own position  
 }
+
+
 
 int main(int argc, char** argv)
 {
@@ -16,11 +28,11 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-
+	//creating subscription on global communication topic		
 	rclcpp::init(argc, argv); 
 	auto node = rclcpp::Node::make_shared("subscriber_node");
 	auto subscription = node->create_subscription<std_msgs::msg::String>(
-			        "/topic", 10,
+			        "/comm_topic", 10,
 				[robotName](const std_msgs::msg::String::SharedPtr message) {
 				callback(message, robotName);
 				});
